d625d921b0ca79008ec36785d31f4318e852889b
Saikumar <saicherrydeadslow@gmail.com>
2024-08-28 20:15:34 +0530

    made home page dynamic

---
+++ js/home.js
@@ -0,0 +1,14 @@
+import data from "../assets/home.json" with {type: "json"};
+
+export const homeJs = ()=>{
+    const moreBtn = document.getElementById("moreBtn"),
+          greetingElm = document.getElementById("greet"),
+          titleElm = document.getElementById("title"),
+          introElm = document.getElementById("intro");
+
+    greetingElm.innerText = data.greet;
+    titleElm.innerHTML = `<span>${data.name[0]}</span> ${data.name[1]}`;
+    introElm.innerHTML = data.introduction;
+    moreBtn.setAttribute("href",data.moreBtn.url);
+    moreBtn.innerText = data.moreBtn.btnText;
+}
---
+++ js/main.js
@@ -0,0 +1,16 @@
+import { navigationPannel } from "./navigationPannel.js";
+import { homeJs } from "./home.js";
+
+const dependencies = ["js/dependencies/jquery-3.5.0.min.js","js/dependencies/styleswitcher.js","js/dependencies/preloader.min.js","js/dependencies/fm.revealator.jquery.min.js","js/dependencies/imagesloaded.pkgd.min.js","js/dependencies/masonry.pkgd.min.js","js/dependencies/classie.js","js/dependencies/cbpGridGallery.js","js/dependencies/jquery.hoverdir.js","js/dependencies/popper.min.js","js/dependencies/bootstrap.js","js/dependencies/custom.js"]
+
+const importDependencies = (files)=>{
+    files.forEach((file)=>{
+        let script = document.createElement('script');
+        script.src = file;
+        document.getElementsByTagName('body')[0].appendChild(script);
+    })
+}
+
+importDependencies(dependencies);
+navigationPannel();
+homeJs();
